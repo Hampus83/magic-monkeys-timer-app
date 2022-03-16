9714730c13b629972db127fa9ecca2522e73afe6
Christian Johnsson <chjohnsson@hotmail.com>
2022-03-16 16:07:46 +0100

    Update analog.js

---
+++ modules/analog.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+import timerValue from './easytimer.min.js'
+
+
+function setDate() {
+    const now = new Date();
+
+  const secondsDegrees = ((timerValue / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+}
+
+  setInterval(setDate, 1000);
+
+setDate();
