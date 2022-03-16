0b679d7c7699dc6cb28195232ca5557079a65a7a
Christian Johnsson <chjohnsson@hotmail.com>
2022-03-16 09:05:40 +0100

    sasa

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const abortBtn = document.querySelectorAll('abort');
+const allPages = document.querySelectorAll('article');
+
+
+
+
+
+
+hideAll()
+
+
+
+
+
+function hideAll(){
+    for (let i = 0; i < allPages.length; i++) {
+        const article = allPages[i];
+        article.classList.add('hidden')
+    }
+}
