e667908609c5718359605fcaa3773f6bbfd3327d
Christian Johnsson <chjohnsson@hotmail.com>
2022-03-16 16:19:15 +0100

    da

--- modules/analog.js
+++ modules/analog.js
@@ -3,18 +3,18 @@ const minsHand = document.querySelector('.min-hand');
 import timerValue from './easytimer.min.js'
 
 
-function setDate() {
-    const now = new Date();
+// function setDate() {
+//     const now = new Date();
 
-  const secondsDegrees = ((timerValue / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+//   const secondsDegrees = ((timerValue / 60) * 360) + 90;
+//   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
 
-  const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-}
+//   const mins = now.getMinutes();
+//   const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+//   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+// }
 
-  setInterval(setDate, 1000);
+//   setInterval(setDate, 1000);
 
-setDate();
+// setDate();
