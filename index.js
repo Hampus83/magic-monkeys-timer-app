187c6fa2be3ea6ade1017f9281d16e6c6b066603
Christian Johnsson <chjohnsson@hotmail.com>
2022-03-16 13:35:14 +0100

    damn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const loadingElem = document.querySelector('.loading-timer')
 const setTimerElem= document.querySelector('.set-timer')
+const startTimerBtn= document.getElementById('start-timer')
 const abortBtn = document.querySelectorAll('.abort');
 const allPages = document.querySelectorAll('article');
 const analogMenu = document.querySelector('#analog_menu');
@@ -9,8 +10,15 @@ const textMenu = document.querySelector('#text_menu');
 const circlesMenu = document.querySelector('#circles_menu')
 const menuElem = document.querySelector('.menu-timer');
 const menuBTN = document.querySelectorAll('.menu-btn');
-const timeIsUpElem = document.querySelector('.alarm-timer')
-let menuChoice = "analog"
+const timeIsUpElem = document.querySelector('.alarm-timer');
+const analogPage = document.querySelector('.analog-timer');
+const digitalPage = document.querySelector('.digital-timer');
+const visualPage = document.querySelector('.visual-timer');
+const textPage = document.querySelector('.text-timer');
+const ringPage = document.querySelector('.ring-timer')
+let menuChoice = "analogPage"
+
+
 console.log(menuChoice)
 hideAll()
 loadingElem.classList.remove('hidden')
@@ -55,31 +63,31 @@ for (let i = 0; i < menuBTN.length; i++) {
 
 ///menyval kanske göra en forloop istället????
 analogMenu.addEventListener('click',()=>
-{menuChoice= "analog";
+{menuChoice= "analogPage";
 console.log(menuChoice)
 menuElem.classList.add('hidden');
 })
 
 digitalMenu.addEventListener('click',()=>
-{menuChoice= "digital";
+{menuChoice= "digitalPage";
 console.log(menuChoice)
 menuElem.classList.add('hidden');
 })
 
 visualMenu.addEventListener('click',()=>
-{menuChoice= "visual";
+{menuChoice= "visualPage";
 console.log(menuChoice)
 menuElem.classList.add('hidden');
 })
 
 textMenu.addEventListener('click',()=>
-{menuChoice= "text";
+{menuChoice= "textPage";
 console.log(menuChoice)
 menuElem.classList.add('hidden');
 })
 
 circlesMenu.addEventListener('click',()=>
-{menuChoice= "circle";
+{menuChoice= "circlesPage";
 console.log(menuChoice)
 menuElem.classList.add('hidden');
 })
@@ -91,4 +99,8 @@ hideAll()
 timeIsUpElem.classList.remove('hidden')
 }
 
-addEventListener
+startTimerBtn.addEventListener('click', ()=> {
+    console.log(this[menuChoice])
+    setTimerElem.classList.add('hidden')
+    window[menuChoice].classList.remove('hidden')
+})
