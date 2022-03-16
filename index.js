94d03125c587ceaa850ec3724666c3999e2c859d
Christian Johnsson <chjohnsson@hotmail.com>
2022-03-16 09:59:58 +0100

    dada

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const abortBtn = document.querySelectorAll('abort');
 const allPages = document.querySelectorAll('article');
-
-
-
-
-
+const analogMenu = document.querySelector('#analog_menu');
+const digitalMenu = document.querySelector('#digital_menu');
+const visualMenu = document.querySelector('#visual_menu');
+const textMenu = document.querySelector('#text_menu');
+const circlesMenu = document.querySelector('#circles_menu')
+const menuElem = document.querySelector('.menu-timer');
+let menuChoice = ""
 
 
 
@@ -17,3 +19,39 @@ function hideAll(){
         article.classList.add('hidden')
     }
 }
+
+
+///menyval
+
+analogMenu.addEventListener('click',()=>
+{menuChoice= "analog";
+console.log(menuChoice)
+menuElem.classList.add('hidden');
+}
+)
+
+digitalMenu.addEventListener('click',()=>
+{menuChoice= "digital";
+console.log(menuChoice)
+menuElem.classList.add('hidden');
+})
+
+visualMenu.addEventListener('click',()=>
+{menuChoice= "visual";
+console.log(menuChoice)
+menuElem.classList.add('hidden');
+})
+
+textMenu.addEventListener('click',()=>
+{menuChoice= "text";
+console.log(menuChoice)
+menuElem.classList.add('hidden');
+})
+
+circlesMenu.addEventListener('click',()=>
+{menuChoice= "circle";
+console.log(menuChoice)
+menuElem.classList.add('hidden');
+})
+
+////
